094bbaaf4ea22ded5f259b25b2ffe6b8ac8ce70c
unknown <wandersonmanto@gmail.com>
2023-01-03 21:36:52 -0300

    add README.md

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function toggleMode(){
   const html = document.documentElement;
 
-  if(html.classList.contains('ligth')){
-    html.classList.remove('ligth');
+  // if(html.classList.contains('ligth')){
+  //   html.classList.remove('ligth');
 
-  } else {
-    html.classList.add('ligth');
+  // } else {
+  //   html.classList.add('ligth');
     
-  }
+  // }
+
+  html.classList.toggle('ligth');
 }
